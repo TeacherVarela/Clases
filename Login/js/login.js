31489dd226492d41da69e73edbe53a719dcddcb0
Sergio Varela <sergio.varela@hilasal.superate.org.sv>
2024-10-08 20:40:52 -0600

    Se agregaron nuevas funcionalidades

--- Login/js/login.js
+++ Login/js/login.js
@@ -3,30 +3,30 @@ const formulario = document.getElementById("formulario");
 formulario.addEventListener('submit', async(e) =>{
     e.preventDefault();
 
-
     const email = formulario.email.value;
     const password = formulario.password.value;
 
-    // Debugging
-    console.log("Email:", email);
-    console.log("Password:", password);
-
     const response = await fetch('https://sheet.best/api/sheets/c983d20b-2b9b-4fbd-8dc4-5cbac94d7789', {
-        method: 'POST',
+        method: 'GET',
         mode: 'cors',
         headers:{
             'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "correo": email,
-            "password": password
-        })
+        }
     })
 
     if(response.ok){
-        alert("Se ha registrado con exito");
-        window.location.href = '../../index.html';
+        const data = await response.json();
+
+        // Busca si existe un registro que coincida con el email y password proporcionados
+        const user = data.find(user => user.correo === email && user.password === password);
+
+        if (user) {
+            alert("Inicio de sesión exitoso");
+            window.location.href = '../../dashboard/dashboard.html';
+        } else {
+            alert("Correo o contraseña incorrectos");
+        }
     }else{
-        alert("Hubo un problema al enviar los datos")
+        alert("Hubo un problema con la base de datos")
     }
 })
---
+++ Registro/js/registro.js
@@ -0,0 +1,27 @@
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+    const email = formulario.email.value;
+    const password = formulario.password.value;
+
+    const response = await fetch('https://sheet.best/api/sheets/c983d20b-2b9b-4fbd-8dc4-5cbac94d7789', {
+        method: 'POST',
+        mode: 'cors',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "correo": email,
+            "password": password
+        })
+    })
+
+    if(response.ok){
+        alert("Se ha registrado con exito");
+        window.location.href = '../../dashboard/dashboard.html';
+    }else{
+        alert("Hubo un problema al enviar los datos")
+    }
+})
